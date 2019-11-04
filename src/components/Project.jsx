710e6affeac9866a474519ea85fe8e5dc490ff3a
pseuyi <pseuyi@gmail.com>
2019-11-04 17:02:22 -0500

    fix router issues

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,45 +1,47 @@
-import React from 'react'
+import React from 'react';
 
-const ReactMarkdown = require('react-markdown')
+const ReactMarkdown = require('react-markdown');
 
-const Project = (props)=>{
-  const projects = props.projects
+const Project = props => {
+  const projects = props.projects;
   return (
     <div className="row">
-      {
-        projects && projects.map(proj=>(
+      {projects &&
+        projects.map(proj => (
           <div
             key={proj.name}
             className="col-xs-12
             col-sm-6
             col-md-6
-            col-lg-4"
-          >
+            col-lg-4">
             <div className="project box ">
               <a href={`${proj.url}`} target="_blank" rel="noopener noreferrer">
-                <img className="thumbnail" src={process.env.PUBLIC_URL + (proj.imgsrc ? proj.imgsrc : `images/${proj.name}.png`)} alt={`${proj.name} thumbnail`} />
+                <img
+                  className="thumbnail"
+                  src={
+                    process.env.PUBLIC_URL +
+                    (proj.imgsrc ? proj.imgsrc : `/images/${proj.name}.png`)
+                  }
+                  alt={`${proj.name} thumbnail`}
+                />
                 <h3>{proj.name}</h3>
               </a>
-              {
-                proj.giturl.length > 0 ?
-                (<a
+              {proj.giturl.length > 0 ? (
+                <a
                   href={proj.giturl}
                   className="octicon"
                   target="_blank"
-                  rel="noopener noreferrer"
-                >
+                  rel="noopener noreferrer">
                   {'< / >'}
-                </a>)
-                : null
-              }
+                </a>
+              ) : null}
               <p className="type">{proj.type}</p>
               <ReactMarkdown className="desc" source={proj.desc} />
             </div>
           </div>
-        ))
-      }
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-import App from './components/App'
-import About from './components/About'
-import Projects from './components/Projects'
-import Notes from './components/Notes'
-import Resume from './components/Resume'
-import './css/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import App from './components/App';
+import About from './components/About';
+import Projects from './components/Projects';
+import Notes from './components/Notes';
+import Resume from './components/Resume';
+import './css/index.css';
 
+const baseUrl = process.env.PUBLIC_URL;
 ReactDOM.render(
-  (
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Projects} />
-        <Route path="about" component={About} />
-        <Route path="projects" component={Projects} />
-        <Route path="more" component={Notes} />
-      </Route>
-      <Route path="cv" component={Resume} />
-    </Router>
-  ),
-document.getElementById('app'))
+  <Router history={browserHistory}>
+    <Route path={baseUrl + '/'} component={App}>
+      <IndexRoute component={Projects} />
+      <Route path={baseUrl + '/about'} component={About} />
+      <Route path={baseUrl + '/projects'} component={Projects} />
+      <Route path={baseUrl + '/more'} component={Notes} />
+    </Route>
+    <Route path="cv" component={Resume} />
+  </Router>,
+  document.getElementById('app'),
+);
